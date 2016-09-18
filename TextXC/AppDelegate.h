2845102f3bf81b63ebbb8642053db78a9c142f11
MissTGao <553924334@qq.com>
2016-09-18 15:43:58 +0800

    Initial Commit

---
+++ TextXC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextXC
+//
+//  Created by iOS_2 on 16/9/18.
+//  Copyright © 2016年 iOS_2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextXC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextXC
+//
+//  Created by iOS_2 on 16/9/18.
+//  Copyright © 2016年 iOS_2. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
